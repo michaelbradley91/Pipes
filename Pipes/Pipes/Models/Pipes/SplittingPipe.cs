d5719dbed10522a67addba7b7e4483ca294c00da
Michael Bradley <michael.bradley@hotmail.co.uk>
2015-12-26 02:44:30 +0000

    Implemented Splitter (AKA Duplicator)

---
+++ Pipes/Pipes/Models/Pipes/SplittingPipe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Pipes.Models.Lets;
+
+namespace Pipes.Models.Pipes
+{
+    public interface ISplittingPipe<TMessage> : ITwoOutletPipe<TMessage>
+    {
+    }
+
+    public class SplittingPipe<TMessage> : TwoOutletPipe<TMessage>, ISplittingPipe<TMessage>
+    {
+        public SplittingPipe(IInlet<TMessage> inlet, IOutlet<TMessage> leftOutlet, IOutlet<TMessage> rightOutlet)
+            : base(inlet, leftOutlet, rightOutlet)
+        {
+        }
+
+        public override Action<TMessage> FindReceiver(IInlet<TMessage> inletSendingMessage)
+        {
+            var leftReceiver = LeftOutlet.FindReceiver();
+            var rightReceiver = RightOutlet.FindReceiver();
+
+            if (leftReceiver == null) return null;
+            if (rightReceiver == null) return null;
+
+            return message =>
+            {
+                leftReceiver(message);
+                rightReceiver(message);
+            };
+        }
+
+        public override Func<TMessage> FindSender(IOutlet<TMessage> outletReceivingMessage)
+        {
+            var sender = Inlet.FindSender();
+            if (sender == null) return null;
+
+            var otherReceiver = outletReceivingMessage == LeftOutlet ? RightOutlet.FindReceiver() : LeftOutlet.FindReceiver();
+            if (otherReceiver == null) return null;
+
+            return () =>
+            {
+                var message = sender();
+
+                otherReceiver(message);
+
+                return message;
+            };
+        }
+    }
+}
