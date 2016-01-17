8e35468dc8749ebb90edc0381da9500dfd7a7de9
Michael Bradley <michael.bradley@hotmail.co.uk>
2016-01-17 15:30:07 +0000

    Added unit tests for tie breaker base classes.

---
+++ Pipes/Pipes.Tests/UnitTests/Models/TieBreakers/TieBreakerTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Pipes.Models.TieBreakers;
+
+namespace Pipes.Tests.UnitTests.Models.TieBreakers
+{
+    [TestFixture]
+    public class TieBreakerTests
+    {
+        private DummyTieBreaker tieBreaker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tieBreaker = new DummyTieBreaker();
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
+        public void ResolveTie_GivenZeroCompetitors_ThrowsAnException()
+        {
+            // Arrange
+            var competitors = new int[0];
+
+            // Act
+            tieBreaker.ResolveTie(competitors);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Test]
+        public void ResolveTie_GivenNullForCompetitors_ThrowsAnException()
+        {
+            // Act
+            tieBreaker.ResolveTie(null);
+        }
+
+        [Test]
+        public void ResolveTie_GivenOneCompetitor_ReturnsThatCompetitor()
+        {
+            // Arrange
+            const int competitor = 3;
+            var competitors = new[] { competitor };
+
+            // Act
+            var result = tieBreaker.ResolveTie(competitors);
+
+            // Assert
+            result.Should().Be(competitor);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
+        public void ResolveTie_GivenDuplicateCompetitors_ThrowsAnException()
+        {
+            // Arrange
+            var competitors = new[] {3, 3};
+
+            // Act
+            tieBreaker.ResolveTie(competitors);
+        }
+
+        [Test]
+        public void ResolveTie_GivenMultipleCompetitors_CallsItsImplementationsResolveMethod()
+        {
+            // Arrange
+            var competitors = new[] {3, 4, 5};
+
+            // Act
+            var result = tieBreaker.ResolveTie(competitors);
+
+            // Assert
+            tieBreaker.PassedCompetitors.ShouldAllBeEquivalentTo(competitors);
+            result.Should().Be(DummyTieBreaker.TieResult);
+        }
+
+        private class DummyTieBreaker : TieBreaker
+        {
+            public int[] PassedCompetitors;
+            public const int TieResult = -1;
+
+            protected override int ResolveTie(int[] tiedCompetitors)
+            {
+                PassedCompetitors = tiedCompetitors;
+                return TieResult;
+            }
+        }
+    }
+}
---
+++ Pipes/Pipes.Tests/UnitTests/Models/TieBreakers/TwoWayTieBreakerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Pipes.Constants;
+using Pipes.Models.TieBreakers;
+
+namespace Pipes.Tests.UnitTests.Models.TieBreakers
+{
+    [TestFixture]
+    public class TwoWayTieBreakerTests
+    {
+        private DummyTwoWayTieBreaker twoWayTieBreaker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            twoWayTieBreaker = new DummyTwoWayTieBreaker();
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
+        public void ResolveTie_GivenMoreThanTwoCompetitors_ThrowsAnException()
+        {
+            // Arrange
+            var competitors = new[] {1, 2, 3};
+
+            // Act
+            twoWayTieBreaker.ResolveTie(competitors);
+        }
+
+        [Test]
+        public void ResolveTie_GivenTwoCompetitorsAndTheTieResultIsLeft_ReturnsTheSmallerCompetitor()
+        {
+            // Arrange
+            var firstCompetitors = new[] {1, 2};
+            var secondCompetitors = new[] {2, 1};
+
+            // Act
+            twoWayTieBreaker.Result = TieResult.Left;
+            var firstResult = twoWayTieBreaker.ResolveTie(firstCompetitors);
+            var secondResult = twoWayTieBreaker.ResolveTie(secondCompetitors);
+
+            // Arrange
+            firstResult.Should().Be(1);
+            secondResult.Should().Be(1);
+        }
+
+        [Test]
+        public void ResolveTie_GivenTwoCompetitorsAndTheTieResultIsRight_ReturnsTheLargerCompetitor()
+        {
+            // Arrange
+            var firstCompetitors = new[] { 1, 2 };
+            var secondCompetitors = new[] { 2, 1 };
+
+            // Act
+            twoWayTieBreaker.Result = TieResult.Right;
+            var firstResult = twoWayTieBreaker.ResolveTie(firstCompetitors);
+            var secondResult = twoWayTieBreaker.ResolveTie(secondCompetitors);
+
+            // Arrange
+            firstResult.Should().Be(2);
+            secondResult.Should().Be(2);
+        }
+
+
+        [Test]
+        public void ResolveTie_GivenOneCompetitor_ReturnsThatCompetitor()
+        {
+            // Arrange
+            const int competitor = 3;
+            var competitors = new[] { competitor };
+
+            // Act
+            var result = twoWayTieBreaker.ResolveTie(competitors);
+
+            // Assert
+            result.Should().Be(competitor);
+        }
+
+        private class DummyTwoWayTieBreaker : TwoWayTieBreaker
+        {
+            public TieResult Result { private get; set; }
+            
+            public override TieResult ResolveTie()
+            {
+                return Result;
+            }
+        }
+    }
+}
